62087dc39417ce705ff4155b0bc409b9cb04f978
Ogunsola Solomon <solomon.eniola7@gmail.com>
2024-08-16 10:32:45 +0100

    fixed a bug

--- build/controller/urlController.d.ts
+++ build/controller/urlController.d.ts
@@ -1,2 +1,3 @@
 export declare const shortenUrl: (req: any, res: any) => Promise<void>;
 export declare const getAllUrl: (req: any, res: any) => Promise<any>;
+export declare const redirectUrl: (req: any, res: any) => Promise<any>;
